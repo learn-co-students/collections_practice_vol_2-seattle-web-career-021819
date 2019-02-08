fd8c7f26d21943ad8a484ffa950d685912e41c61
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-08 03:40:01 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 # your code goes here
+def begins_with_r(array)
+ array.include?("r")
+end
+
+
+def contain_a
+  
+end
+
+def first_wa
+end 
+
+def remove_non_strings
+end 
+
+def count_elements
+end 
+
+def merge_data(keys, data)
+  keys.each do |x|
+    array1 = x[:first_name]
+    puts x
+    array2 = data[0][array1]
+    
+    array2.each do |property,value|
+      x[property] = value
+    
+  end
+end
+  
+  
+  
+  
+  
+end 
+
+def find_cool
+end
+
+def organize_schools
+end 
